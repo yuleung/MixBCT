a4345fee0af0318cb8d8ce8b6a4be0977f318e34
yuleung <leungyu5@outlook.com>
2023-08-14 09:40:26 +0800

    Add files via upload

---
+++ BCT_Methods/AdvBCT/configs/OPdata_ms1mv3_r18_to_r50_AdvBCT_arc_to_arc_f512.py
@@ -0,0 +1,30 @@
+from easydict import EasyDict as edict
+
+config = edict()
+config.loss = "arcface"
+config.network = "r50"
+config.resume = False
+config.output = None
+config.embedding_size = 512
+config.sample_rate = 1.0
+config.fp16 = True
+config.momentum = 0.9
+config.weight_decay = 5e-4
+config.batch_size = 128
+config.dali = False
+config.lr = 0.1
+
+
+config.old_embedding = '../../feature_save/OPdata/f512_r18_arc_data70_feature.npy'
+config.center_radius = '../../feature_save/OPdata/f512_r18_arc_data70_meta_radius_centernorm_for_AdvBCT.npy'
+config.old_embedding_label = '../../feature_save/OPdata/f512_r18_arc_data70_label.npy'
+
+
+
+config.rec_val = '../../dataset/ms1m-retinaface-t1'
+config.rec = '../../dataset/ms1m-retinaface-t1/train_part_70_data'
+config.num_classes = 93431
+config.num_image = 5179510-1554138
+config.num_epoch = 35
+config.warmup_epoch = 0
+config.val_targets = [] #["lfw", "cfp_fp", "agedb_30"]
