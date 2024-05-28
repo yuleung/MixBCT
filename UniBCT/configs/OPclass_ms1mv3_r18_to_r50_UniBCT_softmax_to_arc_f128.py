b2f9553916f7c65a4f6586eb6ec365dd479adf0e
yuleung <leungyu5@outlook.com>
2024-05-28 20:18:20 +0800

    Add files via upload

---
+++ UniBCT/configs/OPclass_ms1mv3_r18_to_r50_UniBCT_softmax_to_arc_f128.py
@@ -0,0 +1,26 @@
+from easydict import EasyDict as edict
+
+config = edict()
+config.loss = "arcface"
+config.network = "r50"
+config.resume = False
+config.output = None
+config.embedding_size = 128
+config.sample_rate = 1.0
+config.fp16 = True
+config.momentum = 0.9
+config.weight_decay = 5e-4
+config.batch_size = 128
+config.lr = 0.1
+
+config.old_model_path = '../../work_dirs/f128_r18_softmax_class30/model.pt'
+config.old_prototype = '../../feature_save/OPclass/f128_r18_softmax_class70_avg_feature.npy'
+
+config.rec_val = '../../dataset/ms1m-retinaface-t1'
+config.rec = '../../dataset/ms1m-retinaface-t1/train_part_70_class'
+config.num_classes = 93431 - 28029
+config.num_image = 5179510 - 1581241
+config.num_epoch = 35
+config.warmup_epoch = -1
+config.decay_epoch = [20, 26, 32]
+config.val_targets =[]
