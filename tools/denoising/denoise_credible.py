1edef8df6c5ffc7966fd46ac81b58ab97fa94321
yuleung <leungyu5@outlook.com>
2025-04-02 11:50:04 +0800

    Update denoise_credible.py

--- tools/denoising/denoise_credible.py
+++ tools/denoising/denoise_credible.py
@@ -4,97 +4,71 @@ from scipy.spatial.distance import cdist
 import math
 import argparse
 import sys
+import os
 
 sys.path.append('./')
 
+def process_chunk(data_chunk, T):
+    if len(data_chunk) == 0:
+        return []
+    
+    avg_feature = np.mean(data_chunk, axis=0).reshape(1, -1)
+    
+    distances = cdist(data_chunk, avg_feature).ravel()
+    sorted_indices = np.argsort(distances)
+    
+    keep_threshold = math.ceil(T * len(data_chunk))
+    true_samples = sorted_indices[:keep_threshold]
+    
+    mask = np.zeros(len(data_chunk), dtype=bool)
+    mask[true_samples] = True
+    return mask.tolist()
 
 def main(args):
     SD = args.SD
-
-    SD_tail = SD.split('.')[0].split('_')[-1]
+    
+    SD_tail = SD.split('_')[-1]
     save_path = './feature_save/'
-    if SD_tail == 'class100':
-        save_path = save_path + 'EXclass/'
-    elif SD_tail == 'data100':
-        save_path = save_path + 'EXdata/'
-    elif SD_tail == 'class70':
-        save_path = save_path + 'OPclass/'
-    elif SD_tail == 'data70':
-        save_path = save_path + 'OPdata/'
-    elif SD_tail == 'class30':
-        save_path = save_path + 'OPclass/'
-    elif SD_tail == 'data30':
-        save_path = save_path + 'OPdata/'
+    if SD_tail in ['class100', 'data100']:
+        save_path = os.path.join(save_path, 'EXclass/' if 'class' in SD_tail else 'EXdata/')
+    else:
+        save_path = os.path.join(save_path, 'OPclass/' if 'class' in SD_tail else 'OPdata/')
 
-    data = np.load(save_path + f'{SD}_feature.npy')
-    labels = np.load(save_path + f'{SD}_label.npy')
+    data = np.load(os.path.join(save_path, f'{SD}_feature.npy'))
+    labels = np.load(os.path.join(save_path, f'{SD}_label.npy'))
 
-    print('data_shape: ', data.shape)
-    print('label_shape: ', labels.shape)
-    data = preprocessing.normalize(data)
-    data = preprocessing.normalize(data, axis=0)
+    data = preprocessing.normalize(data)    
+    data = preprocessing.normalize(data, axis=0) 
 
-    guard = 0
-    count_pre = 0
-    count_last = 0
     credible_label = []
+    current_start = 0
+    current_label = labels[0]
 
-    for label in labels:
-        if label != guard and count_last == len(labels) - 1:
-            assert len(set(labels[count_pre:count_last])) == 1
-            avg_feature = np.sum(data[count_pre:count_last], axis=0) / (count_last - count_pre)
-            avg_feature = avg_feature.reshape(1, -1)
-
-            dis = cdist(data[count_pre:count_last], avg_feature).reshape(-1)
-            sort_dis = np.argsort(dis)
-            true_dis = sort_dis[math.ceil(args.T * len(dis)):].tolist()
-
-            # Denoising Operation
-            dis = np.array([True] * len(dis))
-            if true_dis != []:
-                dis[true_dis] = False
-            credible_label += dis.tolist()
-            credible_label += [True]
-
-        elif count_last == len(labels) - 1:
-            assert len(set(labels[count_pre:count_last + 1])) == 1
-            avg_feature = np.sum(data[count_pre:count_last + 1], axis=0) / (count_last - count_pre)
-            avg_feature = avg_feature.reshape(1, -1)
+    for i in range(1, len(labels)):
+        if labels[i] != current_label:
+            chunk = data[current_start:i]
+            chunk_labels = process_chunk(chunk, args.T)
+            credible_label.extend(chunk_labels)
             
-            # Denoising Operation
-            dis = cdist(data[count_pre:count_last], avg_feature).reshape(-1)
-            sort_dis = np.argsort(dis)
-            true_dis = sort_dis[math.ceil(args.T * len(dis)):].tolist()
-            dis = np.array([True] * len(dis))
-            if true_dis != []:
-                dis[true_dis] = False
-            credible_label += dis.tolist()
+            current_start = i
+            current_label = labels[i]
 
-        elif label != guard:
-            assert len(set(labels[count_pre:count_last])) == 1
-            avg_feature = np.sum(data[count_pre:count_last], axis=0) / (count_last - count_pre)
-            avg_feature = avg_feature.reshape(1, -1)
+    final_chunk = data[current_start:]
+    final_labels = process_chunk(final_chunk, args.T)
+    credible_label.extend(final_labels)
 
-            # Denoising Operation
-            dis = cdist(data[count_pre:count_last], avg_feature).reshape(-1)
-            sort_dis = np.argsort(dis)
-            true_dis = sort_dis[math.ceil(args.T * len(dis)):].tolist()
-            dis = np.array([True] * len(dis))
-            if true_dis != []:
-                dis[true_dis] = False
-            credible_label += dis.tolist()
+    assert len(credible_label) == len(labels), f"Length mismatch: {len(credible_label)} vs {len(labels)}"
 
-            count_pre = count_last
-            guard = label
-        count_last += 1
-
-    np.save(save_path + f'{SD}_credible_ratio_{int(args.T*100)}_feature.npy', credible_label)
-
+    np.save(os.path.join(save_path, f'{SD}_credible_ratio_{int(args.T*100)}_feature.npy'), credible_label)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Denoising and get credible samples.')
-    parser.add_argument('--T', type=float, default=0.9,
-                        help='threthold')
-    parser.add_argument('--SD', type=str,
-                        help='Scenarios detail, For example: f512_r18_arc_class100, f128_r18_softmax_class100')
-    main(parser.parse_args())
+    parser.add_argument('--T', type=float, default=0.9, help='threthold')
+    parser.add_argument('--SD', type=str, help='Scenarios detail, For example: f512_r18_arc_class100, f128_r18_softmax_class100')
+    args = parser.parse_args()
+
+
+    if not 0 <= args.T <= 1:
+        raise ValueError("Threshold T must be in [0, 1]")
+    
+    main(args)
